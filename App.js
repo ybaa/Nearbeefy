e4cd8234c7a949b33714ba07e6e353b48f7d3831
Martyna Łagożna <m.lagozna@live.com>
2017-10-08 17:37:16 +0200

    favourites screen added

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StackNavigator, TabNavigator } from 'react-navigation';
 import store from './store';
 import MainScreen from './screens/MainScreen';
 import AddLocationScreen from './screens/AddLocationScreen';
+import FavouritesScreen from './screens/FavouritesScreen';
 
 export default class App extends Component {
   render() {
@@ -12,11 +13,12 @@ export default class App extends Component {
 
           const Tabs = TabNavigator({
                 Main: { screen: MainScreen },
-                AddLocation: { screen: AddLocationScreen }
+                AddLocation: { screen: AddLocationScreen },
+                Favourites: {screen: FavouritesScreen}
               },{
                 tabBarPosition: 'bottom',
                 tabBarOptions: {
-                  showIcon: true,                  
+                  showIcon: true,
                   style: {
                     backgroundColor: '#4caf50'
                   },
@@ -28,7 +30,8 @@ export default class App extends Component {
 
       const MainNavigator = StackNavigator({
         Main: { screen: Tabs },
-        AddLocation: { screen: AddLocationScreen }
+        AddLocation: { screen: AddLocationScreen },
+        Favourites: {screen: FavouritesScreen}
       });
 
 
---
+++ screens/FavouritesScreen.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { View, Platform, Text, Image} from 'react-native';
+import Expo, { Font} from 'expo';
+import { STATUS_BAR_HEIGHT } from '../constants';
+import icon from '../assets/icons/bigRectangleLogoWithTextTransparent.png';
+import { Icon } from 'react-native-elements';
+
+const cacheImage = images => images.map( (image) => {
+    if(typeof image === 'string')
+      return Image.prefetch(image);
+
+      return Expo.Asset.fromModule(image).downloadAsync();
+});
+
+class FavouritesScreen extends Component {
+  static navigationOptions = () => ({
+    title: 'Favourites',
+    tabBarLabel: 'Favourites',
+    tabBarIcon: ({ tintColor }) => (
+      <Icon
+        name='favorite-border'
+        type='material-icons'
+        size= {25}
+        color='#fff'
+      />
+   ),
+    headerStyle: {
+      height: Platform.OS === 'android' ? 54 + STATUS_BAR_HEIGHT : 54,
+      backgroundColor: '#4caf50'
+    },
+    headerTitleStyle: {
+      marginTop: Platform.OS === 'android' ? STATUS_BAR_HEIGHT : 0,
+      color: 'white'
+    },
+    headerLeft: (
+      <Icon
+        name='menu'
+        type='material-icons'
+        size= {32}
+        color='#fff'
+        style = {styles.imageStyle}
+      />
+    )
+  });
+
+state = {
+  appIsReady: false
+}
+
+async componentDidMount() {
+    await Font.loadAsync({
+      'Quicksand-Light': require('../assets/fonts/Quicksand-Light.ttf'),
+    });
+    this.setState({ fontLoaded: true});
+  }
+
+componentWillMound() {
+  this._loadAssetsAsync();
+}
+
+async _loadAssetsAsync() {
+  const imagesAssets = cacheImage([icon]);
+  await Promise.all([...imagesAssets]);
+  this.setState({
+    appIsReady: true
+  })
+}
+
+
+  render() {
+    return (
+      <View style={{  flex: 1, backgroundColor: '#ddd' }}>
+      <Text>favourites screen</Text>
+
+      </View>
+    );
+  }
+}
+
+const styles = {
+  imageStyle: {
+    marginTop: 25,
+    marginLeft: 10
+  }
+}
+
+export default FavouritesScreen;
